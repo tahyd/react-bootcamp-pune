e6e9b8eace0cfeba299962872f21c8497fe09a2d
tahyd <ta.hyd.003@gmail.com>
2024-06-04 11:39:18 +0530

    initial commit

---
+++ src/Post.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+export default function Post() {
+  // useState
+  const [post,setPost] = useState();
+         
+      const loadPost = ()=>{
+       
+        fetch('https://jsonplaceholder.typicode.com/posts/1')
+        .then(response => response.json())
+        .then(res => {
+               const data = res
+            setPost(data)
+        })
+      }
+  return (
+    <div>Post
+{
+     post?<div>
+        <p>ID : {post.id}</p>
+     </div>:null
+}
+      
+
+<button onClick={loadPost}>Get Post</button>
+
+    </div>
+  )
+}
