6c387893077de32e77968d35405bd0da62010b84
tahyd <ta.hyd.003@gmail.com>
2024-06-05 13:08:59 +0530

    Post modified

--- src/Post.js
+++ src/Post.js
@@ -24,6 +24,7 @@ export default function Post() {
 
 <button onClick={loadPost}>Get Post</button>
 <button onClick={loadPost}>Get Post</button>
+<button onClick={loadPost}>Get Post</button>
     </div>
   )
 }
